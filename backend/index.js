05c8890feebcd93ed3cc87c338c0bdefa31efbb2
FahdAzmy <azmyfahd66@gmail.com>
2024-09-15 11:28:36 +0200

    last change

--- backend/index.js
+++ backend/index.js
@@ -12,29 +12,9 @@ require("dotenv").config();
 
 const app = express();
 
-// CORS middleware
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://salary-tracker-front.vercel.app"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-  next();
-});
 app.use(
   cors({
-    origin: process.env.CORS_ORIGINfly, // استبدل بهذا العنوان إذا كان مختلفاً
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173", // استبدل بهذا العنوان إذا كان مختلفاً
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
