aa5b0b68cbb224fd9584e52c49ec1648003af4a0
FahdAzmy <azmyfahd66@gmail.com>
2024-09-15 09:13:38 +0200

    edit cors options

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,39 @@ app.use(express.json());
 const corsOptions = {
   origin: process.env.CORS_ORIGIN,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-CSRF-Token",
+    "X-Requested-With",
+    "Accept",
+    "Accept-Version",
+    "Content-Length",
+    "Content-MD5",
+    "Date",
+    "X-Api-Version",
+  ],
   credentials: true,
 };
-const Port = process.env.PORT || 4000;
-
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+const Port = process.env.PORT || 4000;
 
 app.use("/api/salarytracker", transactionRoute);
 app.use("/api/salarytracker", userRoute);
